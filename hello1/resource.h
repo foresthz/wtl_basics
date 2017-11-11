71dbc9a46fc8d2dfab1d64265b908321f8c7a61d
f38 <f38@gmail.com>
2017-11-11 15:05:47 +0800

    add button

--- hello1/resource.h
+++ hello1/resource.h
@@ -5,16 +5,19 @@
 #define IDD_ABOUTBOX                    100
 #define IDR_MAINFRAME                   128
 #define IDD_MAINDLG                     129
+#define IDD_NEW_DIALOG                  201
 #define IDC_BUTTON1                     1000
 #define btn_new_message                 1001
+#define btn_new_window                  1002
+#define btn_new_dialog                  1002
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_RESOURCE_VALUE        202
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
